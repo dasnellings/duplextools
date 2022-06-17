7d5626d0aa9e037d14adba1a557a09c43b0c0108
Daniel <daniel.snellings@duke.edu>
2022-06-17 17:00:30 -0400

    barcode

---
+++ barcode/barcode.go
@@ -0,0 +1,186 @@
+package barcode
+
+import (
+	"github.com/vertgenlab/gonomics/dna"
+	"github.com/vertgenlab/gonomics/fastq"
+	"github.com/vertgenlab/gonomics/numbers"
+	"log"
+	"strings"
+)
+
+const McsSharedSequence string = "AGATGTGTATAAGAGACAG"
+
+var mcsSharedBases []dna.Base = dna.StringToBases(McsSharedSequence)
+
+const McsSharedSequenceRevComp string = "CTGTCTCTTATACACATCT"
+
+var mcsSharedBasesRevComp []dna.Base = dna.StringToBases(McsSharedSequenceRevComp)
+
+const McsB1 string = "GGCACCGAAAA"
+const McsB2 string = "CTCGGCGATAAA"
+const McsB3 string = "GGTGGAGCATAA"
+const McsB4 string = "CGAGCGCATTAA"
+const McsB5 string = "AGCCCGGTTATA"
+const McsB6 string = "TCGGCACCAATA"
+const McsB7 string = "GCCTGTGGATTA"
+const McsB8 string = "GCGACCCTTTTA"
+const McsB9 string = "GCATGCGGTAAT"
+const McsB10 string = "GCGTTGCCATAT"
+const McsB11 string = "GGCCGCATTTAT"
+const McsB12 string = "ACCGCCTCTATT"
+const McsB13 string = "CCGTGCCAAAAT"
+const McsB14 string = "TCTCCGGGAATT"
+const McsB15 string = "CCGCGCTTATTT"
+const McsB16 string = "CTGAGCTCGTTTT"
+
+var Barcodes map[string]bool = map[string]bool{
+	McsB1:  true,
+	McsB2:  true,
+	McsB3:  true,
+	McsB4:  true,
+	McsB5:  true,
+	McsB6:  true,
+	McsB7:  true,
+	McsB8:  true,
+	McsB9:  true,
+	McsB10: true,
+	McsB11: true,
+	McsB12: true,
+	McsB13: true,
+	McsB14: true,
+	McsB15: true,
+	McsB16: true,
+}
+
+func Trim(fq *fastq.Fastq) {
+	s := dna.BasesToString(fq.Seq)
+	templateStart := strings.LastIndex(s, McsSharedSequence) + len(McsSharedSequence)
+	templateEnd := strings.Index(s, McsSharedSequenceRevComp)
+	if templateStart == -1 {
+		templateStart = 0
+	}
+	if templateEnd == -1 {
+		templateEnd = len(s)
+	}
+	fq.Seq = fq.Seq[templateStart:templateEnd]
+	fq.Qual = fq.Qual[templateStart:templateEnd]
+}
+
+func Extract(seq []dna.Base) string {
+	s := dna.BasesToString(seq)
+	idx := strings.Index(s, McsSharedSequence)
+	if idx == -1 {
+		return "*"
+	}
+	bc := s[:idx]
+	return Consensus(bc)
+}
+
+// Consensus returns the matching MCS barcode. When an exact match is not found,
+// Levenshtein distance is used to predict what the most probable barcode is.
+// If Levenshtein distance is >2, then Consensus returns "*".
+func Consensus(s string) string {
+	if Barcodes[s] {
+		return s
+	}
+
+	for bc, _ := range Barcodes {
+		if levenshteinString(s, bc) <= 2 {
+			return bc
+		}
+	}
+
+	return "*"
+}
+
+func revComp(base string) string {
+	ans := make([]byte, len(base))
+	var j int
+	for i := len(base) - 1; i >= 0; i-- {
+		switch base[i] {
+		case 'A', 'a':
+			ans[j] = 'T'
+		case 'C', 'c':
+			ans[j] = 'G'
+		case 'G', 'g':
+			ans[j] = 'C'
+		case 'T', 't':
+			ans[j] = 'A'
+		case 'N', 'n':
+			ans[j] = 'N'
+		default:
+			log.Panicf("ERROR: unrecognized base '%s'", base)
+		}
+		j++
+	}
+	return string(ans)
+}
+
+func levenshtein(s1, s2 []dna.Base) int {
+	if len(s1) == 0 || len(s2) == 0 {
+		return numbers.Max(len(s1), len(s2))
+	}
+	s1len := len(s1)
+	s2len := len(s2)
+	column := make([]int, len(s1)+1)
+
+	for y := 1; y <= s1len; y++ {
+		column[y] = y
+	}
+	for x := 1; x <= s2len; x++ {
+		column[0] = x
+		lastkey := x - 1
+		for y := 1; y <= s1len; y++ {
+			oldkey := column[y]
+			var incr int
+			if s1[y-1] != s2[x-1] {
+				incr = 1
+			}
+
+			column[y] = minimum(column[y]+1, column[y-1]+1, lastkey+incr)
+			lastkey = oldkey
+		}
+	}
+	return column[s1len]
+}
+
+func levenshteinString(s1, s2 string) int {
+	if s1 == "" || s2 == "" {
+		return numbers.Max(len(s1), len(s2))
+	}
+	s1len := len(s1)
+	s2len := len(s2)
+	column := make([]int, len(s1)+1)
+
+	for y := 1; y <= s1len; y++ {
+		column[y] = y
+	}
+	for x := 1; x <= s2len; x++ {
+		column[0] = x
+		lastkey := x - 1
+		for y := 1; y <= s1len; y++ {
+			oldkey := column[y]
+			var incr int
+			if s1[y-1] != s2[x-1] {
+				incr = 1
+			}
+
+			column[y] = minimum(column[y]+1, column[y-1]+1, lastkey+incr)
+			lastkey = oldkey
+		}
+	}
+	return column[s1len]
+}
+
+func minimum(a, b, c int) int {
+	if a < b {
+		if a < c {
+			return a
+		}
+	} else {
+		if b < c {
+			return b
+		}
+	}
+	return c
+}
--- mcsFqToBam/mcsFqToBam.go
+++ mcsFqToBam/mcsFqToBam.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/dasnellings/MCS_MS/barcode"
 	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/fastq"
 	"github.com/vertgenlab/gonomics/fileio"
@@ -37,12 +38,19 @@ func fqToSam(fq *fastq.Fastq, sam *sam.Sam, firstInPair bool) {
 	}
 }
 
-func mcsFqToBam(r1File, r2File, outFile string) {
+func mcsFqToBam(r1File, r2File, outFile, missingBcFile string) {
 	readPairs := make(chan fastq.PairedEnd, 1000)
 	go fastq.PairedEndToChan(r1File, r2File, readPairs)
 
 	o := fileio.EasyCreate(outFile)
 	bw := sam.NewBamWriter(o, sam.GenerateHeader(nil, nil, sam.Unsorted, sam.None))
+
+	var noBcFile *fileio.EasyWriter
+	var noBcWriter *sam.BamWriter
+	if missingBcFile != "" {
+		noBcFile = fileio.EasyCreate(outFile)
+		noBcWriter = sam.NewBamWriter(o, sam.GenerateHeader(nil, nil, sam.Unsorted, sam.None))
+	}
 
 	var pair fastq.PairedEnd
 	var s1, s2 sam.Sam
@@ -50,24 +58,57 @@ func mcsFqToBam(r1File, r2File, outFile string) {
 	s2.RName = "*"
 	s1.RNext = "*"
 	s2.RNext = "*"
+	var bcFound bool
+	var bcFor, bcRev, bcId, extra string
 	for pair = range readPairs {
+		bcFor = barcode.Extract(pair.Fwd.Seq)
+		bcRev = barcode.Extract(pair.Rev.Seq)
+		if bcFor > bcRev {
+			bcId = bcFor + "-" + bcRev
+		} else {
+			bcId = bcRev + "-" + bcFor
+		}
+
+		barcode.Trim(&pair.Fwd)
+		barcode.Trim(&pair.Rev)
+
 		fqToSam(&pair.Fwd, &s1, true)
 		fqToSam(&pair.Rev, &s2, false)
 
-		sam.WriteToBamFileHandle(bw, s1, 0)
-		sam.WriteToBamFileHandle(bw, s2, 0)
+		extra = fmt.Sprintf("AL:Z:%s\tBC:Z:%s\tBF:Z:%s\tBR:Z:%s", bcId, bcFor+"-"+bcRev, bcFor, bcRev)
+		s1.Extra = extra
+		s2.Extra = extra
+
+		if bcFound {
+			sam.WriteToBamFileHandle(bw, s1, 0)
+			sam.WriteToBamFileHandle(bw, s2, 0)
+			continue
+		}
+
+		if noBcFile != nil {
+			sam.WriteToBamFileHandle(noBcWriter, s1, 0)
+			sam.WriteToBamFileHandle(noBcWriter, s2, 0)
+		}
 	}
 
 	err := bw.Close()
 	exception.PanicOnErr(err)
 	err = o.Close()
 	exception.PanicOnErr(err)
+
+	if noBcFile != nil {
+		err = noBcWriter.Close()
+		exception.PanicOnErr(err)
+		err = noBcFile.Close()
+		exception.PanicOnErr(err)
+	}
 }
 
 func main() {
 	r1 := flag.String("1", "", "FASTQ file containing R1 reads. May be gzipped.")
 	r2 := flag.String("2", "", "FASTQ file containing R2 reads. May be gzipped.")
 	outfile := flag.String("o", "stdout", "Output BAM file.")
+	missingBcFile := flag.String("missing", "", "Output BAM file for records with missing barcodes")
 	flag.Parse()
 	flag.Usage = usage
 
@@ -76,5 +117,5 @@ func main() {
 		log.Fatalln("ERROR: must input read1 file and read2 file")
 	}
 
-	mcsFqToBam(*r1, *r2, *outfile)
+	mcsFqToBam(*r1, *r2, *outfile, *missingBcFile)
 }
