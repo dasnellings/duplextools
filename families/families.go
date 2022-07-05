f42ab9c0b739d2bc5a5e5905077a2d622cb60412
Daniel <daniel.snellings@duke.edu>
2022-07-05 16:49:19 -0400

    working

--- families/families.go
+++ families/families.go
@@ -13,45 +13,86 @@ func GoAnnotate(reads <-chan sam.Sam, startTolerance int, posMatching bool) <-ch
 }
 
 type family struct {
-	chr      string
-	start    int
-	end      int
-	familyId uint
+	chr           string
+	start         int
+	mateStart     int
+	altMateStarts []int
+	end           int
+	familyId      uint
 }
 
 func annotate(in <-chan sam.Sam, out chan<- sam.Sam, startTolerance int, posMatching bool) {
-	m := make(map[string]family)
+	m := make(map[string]*family)
 	var currFamilyId uint
 	var id string
-	var currFam family
+	var currFam, prevFam, fam *family
+	currFam = new(family)
+	prevFam = new(family)
 	for r := range in {
 		id = getId(barcode.Get(r))
 		if id == "" {
 			out <- r
 			continue
 		}
-		currFam = m[id]
+
+		fam = m[id]
+		if fam != currFam {
+			if currFam.familyId != 0 {
+				prevFam = currFam
+			}
+			currFam = fam
+		}
+
+		if currFam == nil {
+			currFam = new(family)
+			m[id] = currFam
+		}
+
 		switch {
-		case r.RName == currFam.chr && r.GetChromStart() <= currFam.start+startTolerance: // barcode match, part of existing family
+		case posMatching && r.RName == currFam.chr && (r.GetChromStart() == currFam.start || r.GetChromEnd() == currFam.end || int(r.PNext) == currFam.start || r.GetChromStart() == currFam.mateStart): // start/end match, probably part of existing family
+			addFamilyTag(&r, currFam.familyId)
+			if int(r.PNext)-1 != currFam.mateStart {
+				currFam.altMateStarts = append(currFam.altMateStarts, int(r.PNext)-1)
+			}
+
+		case posMatching && r.RName == prevFam.chr && (r.GetChromStart() == prevFam.start || r.GetChromEnd() == prevFam.end || int(r.PNext) == prevFam.start || r.GetChromStart() == prevFam.mateStart): // start/end match, probably part of existing family
+			addFamilyTag(&r, prevFam.familyId)
+			if int(r.PNext)-1 != prevFam.mateStart {
+				prevFam.altMateStarts = append(prevFam.altMateStarts, int(r.PNext)-1)
+			}
+
+		case posMatching && r.RName == currFam.chr && altStartsMatch(r.GetChromStart(), currFam.altMateStarts):
 			addFamilyTag(&r, currFam.familyId)
 
-		case posMatching && r.RName == currFam.chr && (r.GetChromStart() == currFam.start || r.GetChromEnd() == currFam.end): // start/end match, probably part of existing family
+		case posMatching && r.RName == prevFam.chr && altStartsMatch(r.GetChromStart(), prevFam.altMateStarts):
+			addFamilyTag(&r, prevFam.familyId)
+
+		case r.RName == currFam.chr && r.GetChromStart() <= currFam.start+startTolerance: // barcode match, part of existing family
 			addFamilyTag(&r, currFam.familyId)
 
 		default: // must overwrite existing family
 			currFamilyId++
-			currFam = family{
-				chr:      r.RName,
-				start:    r.GetChromStart(),
-				end:      r.GetChromEnd(),
-				familyId: currFamilyId,
-			}
-			m[id] = currFam
+			prevFam = currFam
+			currFam.chr = r.RName
+			currFam.start = r.GetChromStart()
+			currFam.mateStart = int(r.PNext) - 1
+			currFam.altMateStarts = currFam.altMateStarts[:0] // trim
+			currFam.end = r.GetChromEnd()
+			currFam.familyId = currFamilyId
 			addFamilyTag(&r, currFam.familyId)
 		}
 		out <- r
 	}
 	close(out)
+}
+
+func altStartsMatch(start int, altStarts []int) bool {
+	for i := range altStarts {
+		if start == altStarts[i] {
+			return true
+		}
+	}
+	return false
 }
 
 func getId(a, b string) string {
