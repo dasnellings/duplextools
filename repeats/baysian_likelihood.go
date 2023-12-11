f041369639ba97597398a5821aa4a6b84baf37fc
Daniel <daniel.snellings@duke.edu>
2023-12-11 18:12:10 -0500

    save

--- repeats/baysian_likelihood.go
+++ repeats/baysian_likelihood.go
@@ -1,14 +1,61 @@
 package repeats
 
 import (
+	"gonum.org/v1/gonum/stat"
 	"gonum.org/v1/gonum/stat/distuv"
 	"math"
 	"sort"
 )
 
-// BestGenotypeCombo determines the best overall genotype to describe read lengths observed from multiple samples.
-func BestGenotypeCombo(sampleReads [][]int, repeatUnitLength int, poissonLambda, stutterProb, stutterMismatchProb float64) {
+// TestGenotypeFit splits sampleReads by most probable allele of origin (of the alleles in the baseGenotype slice). The output splitReads is organized as splitReads[i][j][k] where i is the sample index, j is the genotype index, and k is the read index.
+// The output ksValues contains the D statistic of the kolmogorov-smirnov test organized as ksValues[i][j] where i is the sample index, and j is the genotype index.
+func TestGenotypeFit(sampleReads [][]int, baseGenotype []int, repeatUnitLength int, poissonLambda, stutterProb, stutterMismatchProb float64, minReadsPerAllele int) (splitReads [][][]int, ksValues [][]float64) {
+	ksValues = make([][]float64, len(sampleReads))
+	splitReads = make([][][]int, len(sampleReads))
+	var i, j int
+	for i = range sampleReads {
+		splitReads[i] = splitReadsByAllele(sampleReads[i], baseGenotype, repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
+		ksValues[i] = make([]float64, len(baseGenotype))
+		for j = range baseGenotype {
+			if len(splitReads[i][j]) < minReadsPerAllele {
+				ksValues[i][j] = 0
+			} else {
+				ksValues[i][j] = ksTest(splitReads[i][j], baseGenotype[j], repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
+			}
+		}
+	}
+	return
+}
 
+// BestSharedGenotype determines the best overall genotype to describe read lengths observed from multiple samples.
+func BestSharedGenotype(sampleReads [][]int, repeatUnitLength int, poissonLambda, stutterProb, stutterMismatchProb float64) (bestGenotypes [][]int, bestBaseGenotype []int, logLikelihoods []float64) {
+	var possibleGenotypes [][]int
+	bestGenotypes = make([][]int, len(sampleReads))
+	var genotype, mergeReads []int
+	for i := range sampleReads {
+		genotype, _ = BestSingleGenotype(sampleReads[i], repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
+		bestGenotypes[i] = genotype
+		if len(genotype) != 0 {
+			possibleGenotypes = append(possibleGenotypes, genotype)
+		}
+		mergeReads = append(mergeReads, sampleReads[i]...)
+	}
+	possibleGenotypes = uniqGenotypes(sortGenotypes(possibleGenotypes))
+	var bestLogLik float64 = math.Inf(-1)
+	var currLogLik float64
+	for i := range possibleGenotypes {
+		currLogLik = GenotypeLogLikelihood(mergeReads, possibleGenotypes[i], repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
+		if currLogLik > bestLogLik {
+			bestLogLik = currLogLik
+			bestBaseGenotype = possibleGenotypes[i]
+		}
+	}
+	logLikelihoods = make([]float64, len(sampleReads))
+	for i := range sampleReads {
+		logLikelihoods[i] = GenotypeLogLikelihood(sampleReads[i], bestBaseGenotype, repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
+	}
+
+	return
 }
 
 // BestSingleGenotype determines the best diploid genotype given the observed read lengths and prior stutter parameters. The possible genotypes are
@@ -17,19 +64,18 @@ func BestGenotypeCombo(sampleReads [][]int, repeatUnitLength int, poissonLambda,
 func BestSingleGenotype(reads []int, repeatUnitLength int, poissonLambda, stutterProb, stutterMismatchProb float64) (genotype []int, logLik float64) {
 	possibleAlleles := unique(reads)
 	possibleGenotypes := permute(possibleAlleles)
-
-	var maxGenotypeIdx int
+	var maxGenotype []int
 	var maxLogLik, currLogLik float64
 	maxLogLik = -math.MaxFloat64
 	for i := range possibleGenotypes {
 		currLogLik = GenotypeLogLikelihood(reads, possibleGenotypes[i], repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
 		if currLogLik > maxLogLik {
 			maxLogLik = currLogLik
-			maxGenotypeIdx = i
+			maxGenotype = possibleGenotypes[i]
 		}
 	}
 
-	return possibleGenotypes[maxGenotypeIdx], maxLogLik
+	return maxGenotype, maxLogLik
 }
 
 // GenotypeLogLikelihood returns the log likelihood of the input genotype given the observed read lengths and the prior stutter probability.
@@ -64,13 +110,13 @@ func likelihoodPerAllele(readLen int, alleleSize int, repeatUnitLength int, pois
 
 	// read length is not a factor of unit length
 	if lengthDiff%repeatUnitLength != 0 {
-		return stutterMismatchProb
+		return stutterMismatchProb / float64(lengthDiff)
 	}
 
 	// read length matches expected stutter profile
 	repeatCountDiff := lengthDiff / repeatUnitLength
 	p := distuv.Poisson{Lambda: poissonLambda}
-	return (stutterProb / 2) * p.Prob(float64(repeatCountDiff))
+	return (stutterProb / 2) * p.Prob(float64(repeatCountDiff-1)) // minus 1 because we want to use the full distribution, the length not changing is not modeled in the poisson.
 }
 
 func abs(i int) int {
@@ -103,4 +149,125 @@ func permute(s []int) [][]int {
 	}
 	ans = append(ans, permute(s[1:])...)
 	return ans
+}
+
+func splitReadsByAllele(reads []int, genotype []int, repeatUnitLength int, poissonLambda, stutterProb, stutterMismatchProb float64) [][]int {
+	//cumLik := make([]float64, len(genotype))
+	ans := make([][]int, len(genotype))
+	var i, j int
+	//var sum, r, curr float64
+	var curr, maxLik float64
+	var maxGenotype int
+	var prevRead int
+	for i = range reads {
+		if reads[i] != prevRead {
+			//sum = 0
+			maxLik = math.Inf(-1)
+			for j = range genotype {
+				curr = likelihoodPerAllele(reads[i], genotype[j], repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
+				if curr > maxLik {
+					maxLik = curr
+					maxGenotype = j
+				}
+				//cumLik[j] = sum + curr
+				//sum += curr
+			}
+		}
+		//r = rand.Float64()
+		//for j = range genotype {
+		//	if r <= cumLik[j]/sum {
+		//		ans[j] = append(ans[j], reads[i])
+		//		break
+		//	}
+		//}
+		prevRead = reads[i]
+		ans[maxGenotype] = append(ans[maxGenotype], reads[i])
+	}
+	return ans
+}
+
+func sortGenotypes(s [][]int) [][]int {
+	sort.Slice(s, func(i, j int) bool {
+		var k int
+		for k = range s[i] {
+			if s[i][k] < s[j][k] {
+				return true
+			}
+			if s[i][k] > s[j][k] {
+				return false
+			}
+		}
+		return true
+	})
+	return s
+}
+
+func uniqGenotypes(s [][]int) [][]int {
+	if len(s) < 2 {
+		return s
+	}
+	ans := make([][]int, 1)
+	ans[0] = s[0]
+	for i := 1; i < len(s); i++ {
+		if !equalGenotypes(s[i], s[i-1]) {
+			ans = append(ans, s[i])
+		}
+	}
+	return ans
+}
+
+func equalGenotypes(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func ksTest(reads []int, alleleSize int, repeatUnitLength int, poissonLambda, stutterProb, stutterMismatchProb float64) float64 {
+	alleleReadsAndWeights := getKsWeights(alleleSize, repeatUnitLength, poissonLambda, stutterProb, stutterMismatchProb)
+	var D float64
+	if len(reads) > 1 {
+		D = stat.KolmogorovSmirnov(intsToFloats(reads), nil, alleleReadsAndWeights[0], alleleReadsAndWeights[1])
+	}
+
+	return D
+}
+
+// TODO Optimize
+func getKsWeights(alleleSize, repeatUnitLen int, lambda, stutterProb, stutterMismatchProb float64) [][]float64 {
+	ans := make([][]float64, 2)
+	ans[0] = make([]float64, 100)
+	ans[1] = make([]float64, 100)
+
+	for i := 0; i < len(ans[0]); i++ {
+		ans[0][i] = float64(i)
+		ans[1][i] = likelihoodPerAllele(i, alleleSize, repeatUnitLen, lambda, stutterProb, stutterMismatchProb)
+	}
+
+	return ans
+}
+
+func intsToFloats(s []int) []float64 {
+	ans := make([]float64, len(s))
+	for i := range s {
+		ans[i] = float64(s[i])
+	}
+	return ans
+}
+
+func genotypesMatch(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
--- repeats/baysian_likelihood_test.go
+++ repeats/baysian_likelihood_test.go
@@ -2,6 +2,12 @@ package repeats
 
 import (
 	"fmt"
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/vcf"
+	"log"
+	"path"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -18,8 +24,135 @@ import (
 //	fmt.Println(unique(s))
 //}
 
+var testFile string = "/Users/danielsnellings/Important/Lab/Walsh/scSTR_genotyping/UMD1255_208_cells/data/UMD1255.vcf.gz"
 var testArr []int = []int{12, 14, 14, 14, 14, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 19, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 22, 22, 23, 24, 24, 24, 24, 26, 26, 26, 26, 26, 26, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 29, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32}
 
+type readVect struct {
+	v     vcf.Vcf
+	names []string
+	reads [][]int
+}
+
+type data struct {
+	splitReads    [][][]int
+	baseGenotype  []int
+	bestGenotypes [][]int
+	ksValues      [][]float64
+}
+
 func TestBestSingleGenotype(t *testing.T) {
-	fmt.Println(BestSingleGenotype(testArr, 2, 0.2, 0.1, 1e-5))
+	repeatUnitLen := 2
+	lambda := 0.8
+	stutterProb := 0.1
+	stutterMismatchProb := 1e-5
+	minReadsPerAllele := 50
+	var d data
+	var i int
+	c := goParseVcfToReadVect(testFile)
+	for v := range c {
+		if v.v.Chr != "chr12" || v.v.Pos != 10591800 {
+			continue
+		}
+		d = getData(v, repeatUnitLen, lambda, stutterProb, stutterMismatchProb, minReadsPerAllele)
+		for i := range v.names {
+			fmt.Printf("Sample: %s\tbestGenotype: %v\tAllele Size: %d\tReads: %d\tD: %.2f\n", path.Base(v.names[i]), d.bestGenotypes[i], d.baseGenotype[0], len(d.splitReads[i][0]), d.ksValues[i][0])
+			fmt.Printf("Sample: %s\tbestGenotype: %v\tAllele Size: %d\tReads: %d\tD: %.2f\n", path.Base(v.names[i]), d.bestGenotypes[i], d.baseGenotype[1], len(d.splitReads[i][1]), d.ksValues[i][1])
+		}
+
+		for i = range v.names {
+			if genotypesMatch(d.bestGenotypes[i], d.baseGenotype) {
+				continue
+			}
+		}
+	}
+}
+
+func getData(r readVect, repeatUnitLen int, lambda, stutterProb, stutterMismatchProb float64, minReadsPerAllele int) data {
+	var ans data
+	ans.bestGenotypes, ans.baseGenotype, _ = BestSharedGenotype(r.reads, repeatUnitLen, lambda, stutterProb, stutterMismatchProb)
+	ans.splitReads, ans.ksValues = TestGenotypeFit(r.reads, ans.baseGenotype, repeatUnitLen, lambda, stutterProb, stutterMismatchProb, minReadsPerAllele)
+	return ans
+}
+
+func goParseVcfToReadVect(file string) <-chan readVect {
+	out := make(chan readVect, 1000)
+	go parseVcfToReadVect(file, out)
+	return out
+}
+
+func parseVcfToReadVect(file string, out chan<- readVect) {
+	records, header := vcf.GoReadToChan(file)
+
+	for v := range records {
+		out <- parseVcf(v, header)
+	}
+	close(out)
+}
+
+func parseVcf(v vcf.Vcf, header vcf.Header) readVect {
+	var ans readVect
+	ans.v = v
+	ans.names = make([]string, len(header.Samples))
+	for key, val := range header.Samples {
+		ans.names[val] = key
+	}
+	ans.reads = make([][]int, len(header.Samples))
+
+	readLengthIndex := getReadLengthsIndex(v.Format)
+	for i := range ans.reads {
+		ans.reads[i] = parseSample(v.Samples[i].FormatData[readLengthIndex])
+	}
+	return ans
+}
+
+func parseSample(s string) []int {
+	if s == "." {
+		return []int{}
+	}
+	words := strings.Split(s, ";")
+	a1 := strings.Split(words[0], ",")
+	a2 := strings.Split(words[1], ",")
+	if a1[0] == "" {
+		a1 = nil
+	}
+	if a2[0] == "" {
+		a2 = nil
+	}
+	merge := append(a1, a2...)
+
+	reads := make([]int, 0)
+	var size, num, i, j int
+	var err error
+	for i = range merge {
+		words = strings.Split(merge[i], "=")
+		size, err = strconv.Atoi(words[0])
+		exception.PanicOnErr(err)
+		num, err = strconv.Atoi(words[1])
+		exception.PanicOnErr(err)
+		for j = 0; j < num; j++ {
+			reads = append(reads, size)
+		}
+	}
+	return reads
+}
+
+func getReadLengthsIndex(format []string) int {
+	var idx int = len(format) - 1
+	if format[idx] == "RL" {
+		return idx
+	}
+
+	for idx = range format {
+		if format[idx] == "RL" {
+			return idx
+		}
+	}
+
+	log.Panic("could not find RL in vcf line")
+	return 0
+}
+
+func simulateData() {
+	//p := distuv.Poisson{Lambda: 0.2}
+
 }
