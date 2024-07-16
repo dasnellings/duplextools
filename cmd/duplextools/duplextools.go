851df2707326c0448d95be34e0f347e52ea25df1
Daniel <daniel.snellings@duke.edu>
2024-07-16 13:57:04 -0400

    rename addReadFamilies to pair

--- cmd/duplextools/duplextools.go
+++ cmd/duplextools/duplextools.go
@@ -16,11 +16,13 @@ type subcommand struct {
 	blurb    string
 }
 
+// SubCommands contains all valid subcommands.
+// New subcommands can be added to duplextools by adding a new entry to this array.
 var SubCommands = []*subcommand{
 	{"extract", runExtract, "extract strand barcodes from fastq"},
-	{"addreadfamilies", runAddReadFamilies, "annotate reads with computed read family"},
+	{"pair", runPair, "annotate reads with computed read family"},
 	{"call", runCall, "call variants from duplex read families"},
-	{"filter", runFilter, "filter variants from `call`"},
+	{"filter", runFilter, "remove likely constitutional variants from callset"},
 	{"burden", runBurden, "calculate adjusted genome-wide mutation burden"},
 }
 
@@ -28,10 +30,12 @@ func usage() {
 	s := new(strings.Builder)
 	s.WriteString(
 		"Program: duplextools (Tools for duplex sequencing data)\n" +
-			"Version: " + version + "\n\n" +
-			"Usage:\tduplextools <command> [options]\n\n" +
+			"Version: " + version + "\n" +
+			"Contact: Daniel Snellings <daniel.snellings@childrens.harvard.edu>\n" +
+			"\nUsage:\tduplextools <command> [options]\n\n" +
 			"Commands:\n")
 
+	// add subcommand text via tabwriter so the columns align
 	w := tabwriter.NewWriter(s, 0, 8, 5, '\t', tabwriter.AlignRight)
 	for i := range SubCommands {
 		fmt.Fprintf(w, "\t%s\t%s\n", SubCommands[i].name, SubCommands[i].blurb)
@@ -40,6 +44,7 @@ func usage() {
 	fmt.Print(s.String())
 }
 
+// commandMap builds a map of possible subcommands keyed on the name of the subcommand
 func commandMap() map[string]func() {
 	m := make(map[string]func())
 	for i := range SubCommands {
@@ -52,11 +57,15 @@ func main() {
 	flag.Usage = usage
 	flag.Parse()
 
+	// check if first argument is a valid subcommand
 	command := commandMap()[flag.Arg(0)]
+
+	// if no command is found, print the usage and return
 	if command == nil {
 		flag.Usage()
 		return
 	}
 
+	// if command successfully found, execute
 	command()
 }
